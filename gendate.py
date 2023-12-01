84d3ae1db04c73c23563ed9bd9fe1d0bbc9d8417
João Tolovi <137790428+joaotolovikeepers@users.noreply.github.com>
2023-12-01 13:56:10 -0300

    Update gendate.py

--- gendate.py
+++ gendate.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import faker
 import random
+
 numero_de_pessoas = 500
 
 fake = faker.Faker()
-df = pd.DataFrame(
-    data=[
-        [fake.name(), fake.random_int(min=1, max=20), fake.boolean()]
-        for _ in range(numero_de_pessoas)
-    ],
-    columns=["name", "zone", "infected"],
-)
 
-df.to_csv("fake_data.csv")
+
+df = pd.DataFrame()
+for _ in range(numero_de_pessoas):
+    df = pd.concat([df, pd.DataFrame([[fake.name(), fake.random_int(min=1, max=20), fake.boolean()]], columns=["name", "zone", "infected"])])
+
+df.to_csv("fake_data.csv", index=False)
+
 
 names = df['name'].tolist()
 friend_pairs = set()
@@ -26,5 +26,10 @@ for name in names:
             friend = random.choice(names)
         friend_pairs.add((name, friend))
         friend_list.append([name, friend])
-df_friends = pd.DataFrame(friend_list, columns=['pessoa1', 'pessoa2'])
+
+
+df_friends = pd.DataFrame(columns=['pessoa1', 'pessoa2'])
+for pair in friend_list:
+    df_friends = pd.concat([df_friends, pd.DataFrame([pair], columns=['pessoa1', 'pessoa2'])])
+
 df_friends.to_csv('friendship_data.csv', index=False)
